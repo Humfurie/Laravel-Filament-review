35579f32e70e49595841bd029d72521022fd728e
Humphrey Singculan <humfurie@gmail.com>
2024-04-22 22:59:09 +0800

    creating category resource tests

--- app/Domain/Category/Models/Category.php
+++ app/Domain/Category/Models/Category.php
@@ -17,8 +17,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Category extends Model
 {
-    use HasFactory, SoftDeletes;
+    use SoftDeletes;
 
+    protected $table = 'categories';
     protected $fillable = [
         'name',
         'slug',
